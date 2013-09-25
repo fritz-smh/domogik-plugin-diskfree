5358e4ef2f97020dacbfbfdd51fd2cc4f99285aa
fritz-smh <fritz.smh@domogik.org>
2013-09-25 16:19:58 +0200

    work on tests

--- tests/02_create_device.py
+++ tests/02_create_device.py
@@ -14,31 +14,43 @@ import requests
 import json
 import sys
 
-### create the device, and if ok, get its id in device_id
-print("Creating the device...")
-response = requests.post("{0}/device/".format(REST_URL), \
-                         headers={'content-type':'application/x-www-form-urlencoded'},
-                         data="name={0}&type=plugin&id=diskfree&host={1}&description=a_description&reference=a_reference&device_type=diskfree.disk_usage".format(DEVICE_NAME, HOST))
-print("Response : [{0}] {1}".format(response.status_code, response.text))
-if response.status_code != 201:
-    print("Error")
-    sys.exit(1)
 
-device = json.loads(response.text)
-device_id = device['id']
-print("Device id is '{0}'".format(device_id))
+def create_device():
+    ### create the device, and if ok, get its id in device_id
+    print("Creating the device...")
+    response = requests.post("{0}/device/".format(REST_URL), \
+                             headers={'content-type':'application/x-www-form-urlencoded'},
+                             data="name={0}&type=plugin&id=diskfree&host={1}&description=a_description&reference=a_reference&device_type=diskfree.disk_usage".format(DEVICE_NAME, HOST))
+    print("Response : [{0}] {1}".format(response.status_code, response.text))
+    if response.status_code != 201:
+        print("Error")
+        sys.exit(1)
+    
+    device = json.loads(response.text)
+    device_id = device['id']
+    print("Device id is '{0}'".format(device_id))
+    
+    
+    ### Just for information, list the parameters for the device_type
+    print("\n\nList the parameters for the device type...")
+    response = requests.get("{0}/device/params/diskfree.disk_usage".format(REST_URL))
+    print("Response : [{0}] {1}".format(response.status_code, response.text))
+    
+    
+    ### And finally, configure the device parameters
+    print("\n\nConfigure the global parameters...")
+    response = requests.put("{0}/device/addglobal/{1}".format(REST_URL, device_id), \
+                             headers={'content-type':'application/x-www-form-urlencoded'},
+                             data="device={0}&interval={1}".format(DEVICE_ADDRESS, DEVICE_INTERVAL))
+    print("Response : [{0}] {1}".format(response.status_code, response.text))
 
 
-### Just for information, list the parameters for the device_type
-print("\n\nList the parameters for the device type...")
-response = requests.get("{0}/device/params/diskfree.disk_usage".format(REST_URL))
-print("Response : [{0}] {1}".format(response.status_code, response.text))
+
+if __name__ == "__main__":
+    create_device()
 
 
-### And finally, configure the device parameters
-print("\n\nConfigure the global parameters...")
-response = requests.put("{0}/device/addglobal/{1}".format(REST_URL, device_id), \
-                         headers={'content-type':'application/x-www-form-urlencoded'},
-                         data="device={0}&interval={1}".format(DEVICE_ADDRESS, DEVICE_INTERVAL))
-print("Response : [{0}] {1}".format(response.status_code, response.text))
-
+# TODO : recup de la config rest
+# TODO : passer les parametres a la fonction
+# TODO : renommer le fichier pour pouvoir importer la fonction en lib
+# TODO : create generic functions for device creation and global parameters
--- tests/10_tests.py
+++ tests/10_tests.py
@@ -3,46 +3,91 @@
 
 from domogik.xpl.common.plugin import XplPlugin
 from domogik.tests.common.plugintestcase import PluginTestCase
+from domogik.tests.common.helpers import check_domogik_is_running
+from domogik.tests.common.helpers import delete_configuration
+from domogik.tests.common.helpers import configure
+#from configure import configure
 import unittest
+import sys
 
 class DiskfreeTestCase(PluginTestCase):
     """ This is the class containing all the tests for the plugin
     """
 
-    def __init__(self, testname, xpl_plugin, name):
+    # this function is the same for all plugins
+    def __init__(self, testname, xpl_plugin, name, configuration):
         """ Constructor
             @param testname : used by unittest to choose the test to launch
             @param xpl_plugin : an instance of XplPlugin to allow to use xPL features 
             @param name : name of the plugin we are testing
+            @param configuration : dict containing the plugin configuration
         """
-        super(DiskfreeTestCase, self).__init__(testname)
+        super(self.__class__, self).__init__(testname)
         self.myxpl = xpl_plugin.myxpl
         self.name = name
+        self.configuration = configuration
         self.get_sanitized_hostname = xpl_plugin.get_sanitized_hostname
 
+    # this function is the same for all plugins
+    def test_0001_domogik_is_running(self):
+        self.assertTrue(check_domogik_is_running())
+
+    # this function is the same for all plugins
+    def test_0010_configure_the_plugin(self):
+        # first, clean the plugin configuration
+        print("Delete the current plugin configuration")
+        self.assertTrue(delete_configuration("plugin", self.name, self.get_sanitized_hostname()))
+        for key in self.configuration:
+            print("Set up configuration : {0} = {1}".format(key, self.configuration[key]))
+            self.assertTrue(configure("plugin", self.name, self.get_sanitized_hostname(), key, self.configuration[key]))
+
+    # this function is the same for all plugins
+    def test_0020_create_the_devices(self):
+        pass
+
+    # this function is the same for all plugins
     def test_9999_hbeat(self):
         print("Check that a heartbeat is sent. This could take up to 5 minutes.")
         self.assertTrue(self.wait_for_xpl(xpltype = "xpl-stat", 
                                           xplschema = "hbeat.app", 
-                                          xplsource = "domogik-diskfree.{0}".format(self.get_sanitized_hostname()),
+                                          xplsource = "domogik-{0}.{1}".format(self.name, self.get_sanitized_hostname()),
                                           timeout = 600))
 
-    def test_200_hbeat(self):
-        self.assertTrue(self.wait_for_xpl(xpltype = "xpl-stat", 
-                                          xplschema = "sensor.basic", 
-                                          timeout = 120))
+    def test_0100_dummy(self):
+        self.assertTrue(True)
 
 
-xpl_plugin = XplPlugin(name = 'test', 
-                       daemonize = False, 
-                       parser = None, 
-                       nohub = True,
-                       test  = True)
-name = "diskfree"
+if __name__ == "__main__":
+    # TODO  : check domogik is up
+    # TODO : allow to bypass hand questions for full auto tests
+    # TODO : configure plugin
+    # TODO : create devices
+    # TODO : start plugin
+    # TODO : ... and check the plugin is in the appropriate status
+    # TODO : add some generic tests about plugin not configured, no devices created and check the plugin status ?
 
-suite = unittest.TestSuite()
-suite.addTest(DiskfreeTestCase("test_9999_hbeat", xpl_plugin, name))
-unittest.TextTestRunner().run(suite)
+    # TODO : create a higher level class which handles all generic tests (00** and 99**)
 
-xpl_plugin.force_leave()
+    # set up the xpl features
+    xpl_plugin = XplPlugin(name = 'test', 
+                           daemonize = False, 
+                           parser = None, 
+                           nohub = True,
+                           test  = True)
+    # set up the plugin name
+    name = "diskfree"
+    # set up the configuration of the plugin
+    # TODO : clean existing configuration
+    cfg = { 'configured' : True }
 
+    
+    # prepare and run the test suite
+    suite = unittest.TestSuite()
+    #suite.addTest(DiskfreeTestCase("test_0001_domogik_is_running", xpl_plugin, name, cfg))
+    suite.addTest(DiskfreeTestCase("test_0010_configure_the_plugin", xpl_plugin, name, cfg))
+    suite.addTest(DiskfreeTestCase("test_0100_dummy", xpl_plugin, name, cfg))
+    #suite.addTest(DiskfreeTestCase("test_9999_hbeat", xpl_plugin, name, cfg))
+    unittest.TextTestRunner().run(suite)
+    
+    xpl_plugin.force_leave()
+    
