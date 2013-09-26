2c0864e286f2a33e141d34b6aa0c8e6827befd20
fritz-smh <fritz.smh@domogik.org>
2013-09-26 11:34:16 +0200

    work on tetss

--- tests/02_create_device.py
+++ tests/02_create_device.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 ### configuration ######################################
 REST_URL="http://192.168.1.10:40405"
@@ -10,9 +11,88 @@ DEVICE_ADDRESS="/home"
 DEVICE_INTERVAL=1
 ########################################################
 
+from domogik.tests.common.helpers import get_rest_url
 import requests
 import json
 import sys
+
+
+class TestDevice():
+    """ Tool to create test devices
+    """
+
+    def __init__(self):
+        """ Construtor
+            @param rest_url : url of the rest server
+        """
+        # rest url
+        self.rest_url = get_rest_url()
+
+        # package informations
+        self.type = None
+        self.id = None
+        self.host = None
+
+        # device informations
+        self.device_name = None
+        self.device_type = None
+
+    def create_device(self, type, id, host, device_name, device_type):
+        """ Call POST /device/... to create the device
+        """
+        # package informations
+        self.type = type
+        self.id = id
+        self.host = host
+        # device informations
+        self.device_name = device_name
+        self.device_type = device_type
+        description = "a test device"
+        reference = "for test only"
+        print("Create a test device for the {0} {1} on {2}. Device type is '{3}', name is '{4}'".format(self.type,
+                                                                                                          self.id,
+                                                                                                          self.host,
+                                                                                                          self.device_type,
+                                                                                                          self.device_name))
+
+        response = requests.post("{0}/device/".format(self.rest_url), \
+            headers={'content-type':'application/x-www-form-urlencoded'},
+            data="name={0}&type={1}&id={2}&host={3}&description={4}&reference={5}&device_type={6}".format(self.device_name,
+                                                                                                          self.type,
+                                                                                                          self.id,
+                                                                                                          self.host,
+                                                                                                          description,
+                                                                                                          reference,
+                                                                                                          self.device_type))
+        print("Response : [{0}] {1}".format(response.status_code, response.text))
+        if response.status_code != 201:
+            raise RuntimeError("Error when creating the device")
+
+        # get the device id for later calls to REST
+        device = json.loads(response.text)
+        self.device_id = device['id']
+        print("The device id is '{0}'".format(self.device_id))
+
+    def configure_global_parameters(self, params):
+        """ Call PUT /device/addglobal/... to set the global parameters for a device
+            @param params : dict of params
+        """
+        print("Configure the global parameters...")
+        # build the data part
+        first = True
+        data = ""
+        for key in params:
+            if first == False:
+                data += "&"
+            else:
+                first = False
+            data += "{0}={1}".format(key, params[key])
+        response = requests.put("{0}/device/addglobal/{1}".format(self.rest_url, self.device_id), \
+                                 headers={'content-type':'application/x-www-form-urlencoded'},
+                                 data="{0}".format(data))
+        print("Response : [{0}] {1}".format(response.status_code, response.text))
+        if response.status_code != 204:
+            raise RuntimeError("Error when configuring the device global parameters")
 
 
 def create_device():
@@ -47,8 +127,17 @@ def create_device():
 
 
 if __name__ == "__main__":
-    create_device()
 
+
+    #print get_rest_url()
+    #create_device()
+
+    td = TestDevice()
+    td.create_device("plugin", "diskfree", "darkstar", "test_device", "diskfree.disk_usage")
+    td.configure_global_parameters({"device" : "/home", "interval" : 1})
+
+
+# TODO : gérer le host dynamiquement (fonction dans helpers ?)
 
 # TODO : recup de la config rest
 # TODO : passer les parametres a la fonction
