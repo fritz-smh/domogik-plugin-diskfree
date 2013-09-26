ae451d04a2b0fb033cce1ad9c5f519aee0f78b6f
fritz-smh <fritz.smh@domogik.org>
2013-09-27 00:13:01 +0200

    tests

--- tests/02_create_device.py
+++ tests/02_create_device.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-### configuration ######################################
-REST_URL="http://192.168.1.10:40405"
-HOST="darkstar"
-DEVICE_NAME="/home on {0}".format(HOST)
-# path in the filesystem for the device 
-DEVICE_ADDRESS="/home"      
-# interval in minutes between each poll
-DEVICE_INTERVAL=1
-########################################################
 
 from domogik.tests.common.helpers import get_rest_url
 import requests
@@ -91,46 +82,11 @@ class TestDevice():
                                  headers={'content-type':'application/x-www-form-urlencoded'},
                                  data="{0}".format(data))
         print("Response : [{0}] {1}".format(response.status_code, response.text))
-        if response.status_code != 204:
+        if response.status_code != 200:
             raise RuntimeError("Error when configuring the device global parameters")
 
 
-def create_device():
-    ### create the device, and if ok, get its id in device_id
-    print("Creating the device...")
-    response = requests.post("{0}/device/".format(REST_URL), \
-                             headers={'content-type':'application/x-www-form-urlencoded'},
-                             data="name={0}&type=plugin&id=diskfree&host={1}&description=a_description&reference=a_reference&device_type=diskfree.disk_usage".format(DEVICE_NAME, HOST))
-    print("Response : [{0}] {1}".format(response.status_code, response.text))
-    if response.status_code != 201:
-        print("Error")
-        sys.exit(1)
-    
-    device = json.loads(response.text)
-    device_id = device['id']
-    print("Device id is '{0}'".format(device_id))
-    
-    
-    ### Just for information, list the parameters for the device_type
-    print("\n\nList the parameters for the device type...")
-    response = requests.get("{0}/device/params/diskfree.disk_usage".format(REST_URL))
-    print("Response : [{0}] {1}".format(response.status_code, response.text))
-    
-    
-    ### And finally, configure the device parameters
-    print("\n\nConfigure the global parameters...")
-    response = requests.put("{0}/device/addglobal/{1}".format(REST_URL, device_id), \
-                             headers={'content-type':'application/x-www-form-urlencoded'},
-                             data="device={0}&interval={1}".format(DEVICE_ADDRESS, DEVICE_INTERVAL))
-    print("Response : [{0}] {1}".format(response.status_code, response.text))
-
-
-
 if __name__ == "__main__":
-
-
-    #print get_rest_url()
-    #create_device()
 
     td = TestDevice()
     td.create_device("plugin", "diskfree", "darkstar", "test_device", "diskfree.disk_usage")
@@ -138,8 +94,5 @@ if __name__ == "__main__":
 
 
 # TODO : gérer le host dynamiquement (fonction dans helpers ?)
-
-# TODO : recup de la config rest
-# TODO : passer les parametres a la fonction
 # TODO : renommer le fichier pour pouvoir importer la fonction en lib
-# TODO : create generic functions for device creation and global parameters
+# TODO : fonction pour supprimer tous les devices avec un nom donné
