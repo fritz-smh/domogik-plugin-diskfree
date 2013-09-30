3b694995eb3429e7341b2ca2827ba0d50a1449b3
fritz-smh <fritz.smh@domogik.org>
2013-09-30 16:07:24 +0200

    tests

--- tests/10_tests.py
+++ tests/10_tests.py
@@ -3,6 +3,7 @@
 
 from domogik.xpl.common.plugin import XplPlugin
 from domogik.tests.common.plugintestcase import PluginTestCase
+from domogik.tests.common.testplugin import TestPlugin
 from domogik.tests.common.testdevice import TestDevice
 from domogik.common.utils import get_sanitized_hostname
 import unittest
@@ -41,15 +42,26 @@ if __name__ == "__main__":
     # TODO
 
     # create a test device
-    td = TestDevice()
-    td.create_device("plugin", "diskfree", get_sanitized_hostname(), "test_device_diskfree", "diskfree.disk_usage")
-    td.configure_global_parameters({"device" : "/home", "interval" : 1})
-    
-    # prepare and run the test suite
+    #td = TestDevice()
+    #td.create_device("plugin", "diskfree", get_sanitized_hostname(), "test_device_diskfree", "diskfree.disk_usage")
+    #td.configure_global_parameters({"device" : "/home", "interval" : 1})
+
+    ### prepare and run the test suite
     suite = unittest.TestSuite()
+    # check domogik is running, configure the plugin
     suite.addTest(DiskfreeTestCase("test_0001_domogik_is_running", xpl_plugin, name, cfg))
     suite.addTest(DiskfreeTestCase("test_0010_configure_the_plugin", xpl_plugin, name, cfg))
+    
+    # start the plugin
+    # TODO : move in PluginTestCase and create some tests : test_0020
+    tp = TestPlugin("diskfree", "darkstar")
+    tp.request_startup()
+    tp.wait_for_startup()
+
+    # do the specific plugin tests
     suite.addTest(DiskfreeTestCase("test_0100_dummy", xpl_plugin, name, cfg))
+
+    # do some tests comon to all the plugins
     suite.addTest(DiskfreeTestCase("test_9999_hbeat", xpl_plugin, name, cfg))
     unittest.TextTestRunner().run(suite)
     
